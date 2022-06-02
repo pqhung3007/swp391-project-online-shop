e62205ec58affa748abc171ec0f50de3aa21e13e
Windows 10 <hungpham307@gmail.com>
2022-06-02 18:57:36 +0700

    first commit, migrate from old repo

---
+++ src/java/context/DBContext.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package context;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Administrator
+ */
+public class DBContext {
+
+    protected Connection connection;
+
+    public DBContext() {
+        try {
+            String user = "sa";
+            String pass = "123456";
+            String url = "jdbc:sqlserver://HungPC:1433;databaseName=OnlineShopSystem";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, user, pass);
+
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        DBContext dBContext = new DBContext();
+        if (dBContext.connection != null) {
+            System.out.println("ok");
+        } else {
+            System.out.println("fail");
+        }
+    }
+
+}
