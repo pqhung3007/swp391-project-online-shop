fc38c1096cd8d119f3e80f70d8db6a9dc913619a
Windows 10 <hungpham307@gmail.com>
2022-06-21 12:16:07 +0700

    display image on link paste & get categories

--- build/web/addProduct.jsp
+++ build/web/addProduct.jsp
@@ -1,10 +1,7 @@
-<%-- 
-    Document   : addProduct
-    Created on : 20-Jun-2022, 10:29:59
-    Author     : Administrator
---%>
+<%-- Document : addProduct Created on : 20-Jun-2022, 10:29:59 Author : Administrator --%>
 
-<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@page contentType="text/html" pageEncoding="UTF-8" %>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -16,34 +13,46 @@
         <link href="https://fonts.googleapis.com/icon?family=Material+Symbols+Outlined" rel="stylesheet">
     </head>
     <body>
-        <div class="container">
+        <div class="container-product">
             <!-- VERTICAL NAVBAR -->
             <%@include file="components/sidebar.jsp" %>
             <!-- MAIN SECTION -->
-            <main>
+            <div style="margin-top: 2rem">
                 <h1>Add new product</h1>
                 <form action="action">
                     <div class="input-product">
                         <div class="input-image">
-                            <input class="field" type="text" placeholder="Product Image URL">
-                            <img src="img/service-2.jpg" alt="">
+                            <input class="field image-name" type="text" placeholder="Product Image URL" name="image">
+                            <img id="image" />
                         </div>
                         <div class="input-fields">
-                            <input class="field" type="text" placeholder="Product Name">
-                            <input class="field" type="text" placeholder="Product Price">
-                            <input class="field" type="text" placeholder="Product Quantity">
+                            <input class="field" type="text" placeholder="Product Name" name="name">
+                            <input class="field" type="number" placeholder="Product Price (VND)" name="price">
+                            <select name="category" class="field">
+                                <c:forEach items="${categories}" var="c">
+                                    <option>${c.categoryName}</option>
+                                </c:forEach>
+                            </select>
                             <fieldset class="info">
                                 <legend>Product Description</legend>
                                 <textarea></textarea>
                             </fieldset>
-                            <input class="btn-submit" type="submit" value="Save"> 
+                            <input class="btn-submit" type="submit" value="Save">
                         </div>
                     </div>
                 </form>
-            </main>
+            </div>
 
         </div>
 
         <script src="app.js"></script>
+        <script>
+            const pictureURL = document.querySelector('.image-name');
+            function changePicture() {
+                document.querySelector('#image').src = pictureURL.value;
+            }
+            pictureURL.addEventListener("input", changePicture)
+        </script>
+
     </body>
 </html>
--- src/java/controller/AddProductController.java
+++ src/java/controller/AddProductController.java
@@ -2,41 +2,53 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller;
 
+import dao.CategoryDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.util.ArrayList;
+import model.Category;
 
 /**
  *
  * @author Administrator
  */
 public class AddProductController extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
+        try ( PrintWriter out = response.getWriter()) {
+            CategoryDAO category = new CategoryDAO();
+            ArrayList<Category> categoryList = category.getAll();
+
+            HttpSession session = request.getSession();
+            session.setAttribute("categories", categoryList);
+            
+            System.out.println(categoryList);
             request.getRequestDispatcher("addProduct.jsp").forward(request, response);
         }
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -44,12 +56,13 @@ public class AddProductController extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
-    } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -57,12 +70,13 @@ public class AddProductController extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
