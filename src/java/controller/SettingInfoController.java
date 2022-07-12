054cc06ab949dcf3864104fe0036426b1caaab79
Windows 10 <hungpham307@gmail.com>
2022-07-12 18:25:30 +0700

    fix: update info for seller & shipper

--- src/java/controller/SettingInfoController.java
+++ src/java/controller/SettingInfoController.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller;
 
 import dao.AccountDAO;
@@ -20,27 +19,30 @@ import model.User;
  * @author Administrator
  */
 public class SettingInfoController extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
+        try ( PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-            
+
             request.getRequestDispatcher("userSettings.jsp").forward(request, response);
         }
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -48,12 +50,13 @@ public class SettingInfoController extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
-    } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -61,9 +64,9 @@ public class SettingInfoController extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-         Account account = (Account) request.getSession().getAttribute("account");
+        Account account = (Account) request.getSession().getAttribute("account");
         int accountId = account.getAccountId();
         String phone = request.getParameter("phone");
         String name = request.getParameter("name");
@@ -71,12 +74,13 @@ public class SettingInfoController extends HttpServlet {
         String email = request.getParameter("email");
         AccountDAO dao = new AccountDAO();
         //nhows update
-        
-        
+        dao.updateUser(name, phone, address, email, accountId);
+        request.getRequestDispatcher("seller-dashboard").forward(request, response);
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
