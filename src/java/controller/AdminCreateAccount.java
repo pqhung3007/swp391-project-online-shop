a68ff8fd212eb8fc52c91adc0370aeec15789b19
Windows 10 <hungpham307@gmail.com>
2022-07-21 10:16:28 +0700

    fetch actors to create for admin

--- src/java/controller/AdminCreateAccount.java
+++ src/java/controller/AdminCreateAccount.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller;
 
 import dao.AccountDAO;
@@ -14,33 +13,37 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.util.List;
 import model.Role;
+import org.mindrot.jbcrypt.BCrypt;
 
 /**
  *
  * @author Le Viet
  */
 public class AdminCreateAccount extends BaseAuthController {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        
-        AccountDAO  a = new AccountDAO();
-        List<Role> allRole = a.getAllRole(1);
+            throws ServletException, IOException {
+
+        AccountDAO a = new AccountDAO();
+        List<Role> allRole = a.getAllRole(2);
         request.setAttribute("roles", allRole);
         request.getRequestDispatcher("createAccountAdmin.jsp").forward(request, response);
-    
-    } 
+
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -48,12 +51,13 @@ public class AdminCreateAccount extends BaseAuthController {
      */
     @Override
     protected void processGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
-    } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -61,17 +65,19 @@ public class AdminCreateAccount extends BaseAuthController {
      */
     @Override
     protected void processPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-       int roleID =  Integer.parseInt(request.getParameter("roles"));
-       String username = request.getParameter("username");
-       String password = request.getParameter("password");
-       
-       new AccountDAO().createAccount(roleID,username,password);
-       response.sendRedirect("create-account");
+            throws ServletException, IOException {
+        int roleID = Integer.parseInt(request.getParameter("roles"));
+        String username = request.getParameter("username");
+        String rawPassword = request.getParameter("password");
+
+        String password = BCrypt.hashpw(rawPassword, BCrypt.gensalt(10));
+        new AccountDAO().createAccount(roleID, username, password);
+        response.sendRedirect("create-account");
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
